e77e5f9975319402bc0c2e04bcc6fd291d373bf6
Sean Clayton <sclayton@unc.edu>
2022-01-20 18:20:32 -0500

    EX01 - Midway

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -4,3 +4,20 @@ __author__ = "730358979"
 
 guess_word: str = input("Enter a 5-character word: ")
 single_character: str = input("Enter a single character: ")
+
+print("Searching for " + single_character + " in " + guess_word)
+
+if single_character == guess_word[0]:
+    print(single_character + " found at index 0")
+
+if single_character == guess_word[1]:
+    print(single_character + " found at index 1")
+
+if single_character == guess_word[2]:
+    print(single_character + " found at index 2")
+
+if single_character == guess_word[3]:
+    print(single_character + " found at index 3")
+
+if single_character == guess_word[4]:
+    print(single_character + " found at index 4")
