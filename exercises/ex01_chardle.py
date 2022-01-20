e2c96ab850ff34812824ce2c8662d1ac98f9d8b7
Sean Clayton <sclayton@unc.edu>
2022-01-20 00:24:29 -0500

    Started EX01

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,6 @@
+"""Exercise 01 for COMP110 - Chardle."""
+
+__author__ = "730358979"
+
+guess_word: str = input("Enter a 5-character word: ")
+single_character: str = input("Enter a single character: ")
