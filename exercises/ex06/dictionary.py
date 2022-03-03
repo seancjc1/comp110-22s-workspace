f5f2b82f796ea229dcc34a77abd24f81134d4415
Sean Clayton <sclayton@unc.edu>
2022-03-03 18:25:27 -0500

    Finished EX06

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -6,16 +6,36 @@ __author__ = "730358979"
 def invert(dictionary: dict[str, str]) -> dict[str, str]:
     """Inverts the keys and values of a given dictionary."""
     inversion: dict[str, str] = dict()
+    for key in dictionary:
+        if dictionary[key] in inversion: 
+            raise KeyError("No") 
+        inversion[dictionary[key]] = key
     return inversion
 
 
 def favorite_color(colors: dict[str, str]) -> str:
     """Determines favorite color based on frequency in given dictionary."""
-    color: str = str()
+    color: str = ""
+    favorites: dict[str, int] = dict()
+    for key in colors:
+        if colors[key] in favorites:
+            favorites[colors[key]] += 1
+        else:
+            favorites[colors[key]] = 1
+    counter: int = 0
+    for key in favorites:
+        if favorites[key] > counter:
+            counter = favorites[key]
+            color = key
     return color
 
 
-def count(list: list[str]) -> dict[str, int]:
+def count(given_list: list[str]) -> dict[str, int]:
     """Calculate number of times a string appears in a given list."""
     frequency: dict[str, int] = dict()
+    for string in given_list:
+        if string in frequency:
+            frequency[string] += 1
+        else:
+            frequency[string] = 1
     return frequency
--- exercises/ex06/dictionary_test.py
+++ exercises/ex06/dictionary_test.py
@@ -6,12 +6,54 @@ from dictionary import invert, favorite_color, count
 
 
 def test_invert() -> None:
-    """Tests the invert function."""
+    """Tests the invert function's main use."""
+    ys: dict[str, str] = {"a": "z", "b": "y", "c": "x"}
+    assert invert(ys) == {"z": "a", "y": "b", "x": "c"}
+
+
+def test_invert_single() -> None:
+    """Tests the invert function when given a dictionary with a single key-value pair."""
+    ys: dict[str, str] = {"hootie": "blowfish"}
+    assert invert(ys) == {"blowfish": "hootie"}
+
+
+def test_invert_empty() -> None:
+    """Tests the invert function when given an empty dictionary."""
+    ys: dict[str, str] = dict()
+    assert invert(ys) == {}
 
 
 def test_fav_color() -> None:
-    """Test the favorite color function."""
+    """Tests the favorite color function's main use."""
+    ys: dict[str, str] = {"john": "blue", "jane": "green", "sean": "green"}
+    assert favorite_color(ys) == "green"
+
+
+def test_fav_color_same_number() -> None:
+    """Tests the favorite color function when given a dictionary with a single key-value pair."""
+    ys: dict[str, str] = {"john": "blue", "jane": "blue", "sean": "green", "lara": "green"}
+    assert favorite_color(ys) == "blue"
+
+
+def test_fav_color_empty() -> None:
+    """Tests the favorite color function when given an empty dictionary."""
+    ys: dict[str, str] = {}
+    assert favorite_color(ys) == ""
 
 
 def test_count() -> None:
-    """Tests the count function."""
+    """Tests the count function's main use."""
+    xs: list[str] = ["alex", "jake", "sean", "sean"]
+    assert count(xs) == {"alex": 1, "jake": 1, "sean": 2}
+
+
+def test_count_single() -> None:
+    """Tests the count function when given a list of a single item."""
+    xs: list[str] = ["alex"]
+    assert count(xs) == {"alex": 1}
+
+
+def test_count_empty() -> None:
+    """Tests the count function when given an empty list."""
+    xs: list[str] = []
+    assert count(xs) == {}
